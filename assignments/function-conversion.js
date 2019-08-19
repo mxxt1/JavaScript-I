49de01d44bb21f5db287999d75e7a17bbbc0dad0
mxxt1 <matt@dustcloudlabs.com>
2019-08-19 13:59:57 -0400

    function_conversion mvp

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,42 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+    return param;
+};
+anotherFunction();
+console.log(anotherFunction("test"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1,param2) =>{
+    return param1 + param2;
+};
+console.log(add(1,2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (param1,param2) =>{
+    return param1 - param2;
+};
+console.log(subtract(1,2));
 
 
 // Stretch
@@ -28,3 +50,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+// givenArray = [1,2,3,4];
+// const triple = (givenArray.map(function(num))) =>{
+//     return num*3;
+// };
+// console.log(triple)
